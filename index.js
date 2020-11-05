f8d653aa65d3d90fc76524e85ec0c91b0fe84544
surus777 <001pst@gmail.com>
2020-11-05 10:00:01 +0200

    telegrambot

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
+try {require("./config") } catch {  }
 const http = require("http");
 const server = http.createServer(function (request, response) {
     if (request.url == '/pages/about') response.end("it's a web server")
     else response.end("hello world");
 
 });
-server.listen(process.env.PORT || 3000, function ( ){
-    console.log("server started")
+server.listen(process.env.PORT || 3000, function () {
+    console.log("server started");
+    bot.log("server started");    
+
 });
+
+const TelegramBot = require('node-telegram-bot-api')
+
+let bot, adminId
+
+
+
+bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true })
+
+bot.on('message', msg =>
+    bot.sendMessage(msg.chat.id, 'Whatever your goal is, you can do it!'))
+
+bot.on('polling_error', err =>
+    process.env.PORT ? process.exit(1) : console.error(err.message))
+
+adminId = +process.env.ADMIN_CHAT
+
+
+
+bot.log = function (...args) {
+    args.forEach(arg => {
+        if (typeof arg != 'string') arg = '`' + JSON.stringify(arg, null, 2) + '`'
+        bot.sendMessage(adminId, arg, { parse_mode: 'markdown' })
+    })
+}
